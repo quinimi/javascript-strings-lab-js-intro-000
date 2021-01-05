e7abb75a7e4a9d1e5a2a368f4a1b4f9987800b2f
Quin Mixon <qmixon@live.com>
2021-01-05 05:43:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello" + specialGuest + "!";
 
-var topic = "Let\'sspace";
+var topic = "Let\'s taspace";
 
 var conversation = `${topic}`;
